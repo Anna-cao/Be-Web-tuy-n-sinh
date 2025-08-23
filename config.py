fb51ee56400fb9b4634778a8b4097ef9e0879790
Anna-cao <caohagt@gmail.com>
2025-08-24 02:04:56 +0700

    Update config

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = (
-        "postgresql://postgres:123456@localhost:5432/DuAn"
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "postgresql+psycopg2://postgres:123456@localhost:5432/DuAn"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
